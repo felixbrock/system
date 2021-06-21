2ca7848045a9102a84593da23f2b593efcaf0cb5
Felix Brockmeier <felix.brockmeier@outlook.com>
2021-06-21 09:41:44 +0100

    moved systemId parm to request query

--- src/infrastructure/api/controllers/create-warning-controller.ts
+++ src/infrastructure/api/controllers/create-warning-controller.ts
@@ -1,5 +1,6 @@
 // TODO: Violation of control flow. DI for express instead
 import { Request, Response } from 'express';
+import Result from '../../../domain/value-types/transient-types';
 import { CreateWarning, CreateWarningRequestDto, CreateWarningResponseDto } from '../../../domain/warning/create-warning';
 import { BaseController, CodeHttp } from '../../shared';
 
@@ -11,14 +12,27 @@ export default class CreateWarningController extends BaseController {
     this.#createWarning = createWarning;
   }
 
-  #buildRequestDto = (httpRequest: Request): CreateWarningRequestDto => ({
-      systemId: httpRequest.body.systemId,
-    })
+  #buildRequestDto = (
+    httpRequest: Request
+  ): Result<CreateWarningRequestDto> => {
+    const { systemId } = httpRequest.query;
+    if (typeof systemId === 'string')
+      return Result.ok<CreateWarningRequestDto>({
+        systemId,
+      });
+    return Result.fail<CreateWarningRequestDto>(
+      'request query parameter subscriptionId is supposed to be in string format'
+    );
+  };
 
   protected async executeImpl(req: Request, res: Response): Promise<Response> {
     try {
-      const requestDto : CreateWarningRequestDto = this.#buildRequestDto(req);
-      const useCaseResult : CreateWarningResponseDto = await this.#createWarning.execute(requestDto);
+      const buildDtoResult: Result<CreateWarningRequestDto> = this.#buildRequestDto(req);
+
+      if(buildDtoResult.error) return CreateWarningController.badRequest(res, buildDtoResult.error);
+      if(!buildDtoResult.value) return CreateWarningController.badRequest(res, 'Invalid request query paramerters');
+
+      const useCaseResult : CreateWarningResponseDto = await this.#createWarning.execute(buildDtoResult.value);
 
       if (useCaseResult.error) {
         return CreateWarningController.badRequest(res, useCaseResult.error);
