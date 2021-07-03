be2d8f8db2c69606c0ad7c7c77893acfec360a8f
Felix Brockmeier <felix.brockmeier@outlook.com>
2021-07-03 15:46:00 +0100

    minor fix

--- src/domain/system/system-dto.ts
+++ src/domain/system/system-dto.ts
@@ -1,16 +1,18 @@
 import { System } from '../entities';
-import { WarningDto } from '../warning/warning-dto';
+import { buildWarningDto, WarningDto } from '../warning/warning-dto';
 
 export interface SystemDto {
   id: string;
   name: string;
   warnings: WarningDto[];
   modifiedOn: number;
-};
+}
 
 export const buildSystemDto = (system: System): SystemDto => ({
   id: system.id,
   name: system.name,
-  warnings: system.warnings,
+  warnings: system.warnings.map(
+    (warning): WarningDto => buildWarningDto(warning)
+  ),
   modifiedOn: system.modifiedOn,
 });
