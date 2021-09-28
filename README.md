17c17888a5d705f3c400e950b6a887219ee9e0a6
Felix Brockmeier <felix.brockmeier@outlook.com>
2021-09-28 11:25:18 +0200

    new deploy command

--- README.md
+++ README.md
@@ -1,15 +1,1 @@
 # system
-
-------------------------------
-
-Docker deployment
-
-aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 085009017826.dkr.ecr.eu-central-1.amazonaws.com
-
-$serviceName = 'system'; $env='test'; $version = 'v1.0.6'; $previousVersion = 'v1.0.5';
-
-$name = "085009017826.dkr.ecr.eu-central-1.amazonaws.com/${serviceName}"; npm run build; $tag = $(If ($env -eq 'production') {${version}} Else {'latest'}); docker build --build-arg "ENV=${env}" -t "${name}:${tag}" .; docker push "${name}:${tag}";
-
-If ($env -eq 'production') {docker rmi "${name}:${previousVersion}"}; If ($env -eq 'production') {aws ecr batch-delete-image --repository-name $serviceName --image-ids "imageTag=${previousVersion}"}
-
-If ($env -eq 'test') {aws ecs update-service --cluster hivedive-test --service "${serviceName}-service" --force-new-deployment};
---
+++ deploy.ps1
@@ -0,0 +1,26 @@
+aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 085009017826.dkr.ecr.eu-central-1.amazonaws.com;
+
+$serviceName = 'system'
+$env='test'
+$version = 'v1.0.7'
+$previousVersion = 'v1.0.6'
+
+$name = "085009017826.dkr.ecr.eu-central-1.amazonaws.com/${serviceName}"
+npm run build
+$tag = $(if ($env -eq 'production') {${version}} else {'latest'})
+docker build --build-arg "ENV=${env}" -t "${name}:${tag}" .
+docker push "${name}:${tag}"
+
+if ($env -eq 'production') {
+  docker rmi "${name}:${previousVersion}"
+  aws ecr batch-delete-image --repository-name $serviceName --image-ids "imageTag=${previousVersion}"
+}
+elseif ($env -eq 'test') {
+  docker image prune -f
+  # $imagesToDelete = aws ecr list-images --repository-name $serviceName --filter "tagStatus=UNTAGGED" --query 'imageIds[*]' --output json
+  # aws ecr batch-delete-image --repository-name $serviceName --image-ids "$imagesToDelete"
+
+  aws ecs update-service --cluster hivedive-test --service "${serviceName}-service" --force-new-deployment
+}
+
+# docker run -dp 3000:3000 085009017826.dkr.ecr.eu-central-1.amazonaws.com/selector:v1.0.0
