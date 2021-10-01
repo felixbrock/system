abb6d32266177a0a904c8bfcb40527310dc22632
Felix Brockmeier <felix.brockmeier@outlook.com>
2021-10-01 10:42:16 +0200

    test console.log

--- src/infrastructure/api/routes/v1.ts
+++ src/infrastructure/api/routes/v1.ts
@@ -8,7 +8,10 @@ const version = 'v1';
 
 const v1Router = Router();
 
-v1Router.get('/', (req, res) => res.json({ message: "Yo! We're up!" }));
+v1Router.get('/', (req, res) => {
+  console.log(req);
+  
+  return res.json({ message: "Yo! We're up!" });});
 
 v1Router.use(`/${apiRoot}/${version}/system`, systemRoutes);
 
